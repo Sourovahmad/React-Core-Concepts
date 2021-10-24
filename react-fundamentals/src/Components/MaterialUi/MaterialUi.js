9467d6627f25fb94cc670e4c0b936e51ea6f1c51
sourovahmad <mdsourovahmad19@gmail.com>
2021-10-24 19:26:40 +0600

    material ui

---
+++ react-fundamentals/src/Components/MaterialUi/MaterialUi.js
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+
+const MaterialUi = () => {
+    return (
+        <div>
+            <Button variant="contained" color="info">Hello Button</Button>
+        </div>
+    );
+};
+
+export default MaterialUi;
