0feac8da08ce07343bf675b76d8172e6c89ebbbb
sourovahmad <mdsourovahmad19@gmail.com>
2021-10-24 21:38:30 +0600

    axios install and practce with get

--- react-fundamentals/src/Components/MaterialUi/MaterialUi.js
+++ react-fundamentals/src/Components/MaterialUi/MaterialUi.js
@@ -1,8 +1,16 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect } from 'react';
 
 
 const MaterialUi = () => {
+
+    useEffect(() => {
+
+        Axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(data => console.log(data.data));
+    }, [])
+
     return (
         <div>
             <Button variant="contained" color="info">Hello Button</Button>
